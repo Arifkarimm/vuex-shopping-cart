08eed7f9e3456e367fd6efa70d7a713526324e9b
Fazlul <fkarif.cse@gmail.com>
2020-04-21 17:15:06 +0200

    product list component added

---
+++ src/components/ProductList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>ProductList</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductList"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
